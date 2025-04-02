8493d48ac0682b9884c77522d3c4a397e0f74c34
Giordanni <giordanniformiga103@gmail.com>
2025-04-02 15:42:29 -0300

    Iniciando conexao com o banco de dados

--- src/database/db.js
+++ src/database/db.js
@@ -1,21 +1,23 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
     host: process.env.DB_HOST,
-    port: process.env.DB_USER,
-    database: process.dotenv.DB_NAME,
+    port: process.env.DB_PORT,
+    database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
 });
 
 const conectDataBase = async () => {
     try {
-        console.log('Connecting to database...');
+        console.log('\nConnecting to database...');
         await pool.connect();
-        console.log('Connected to database');
+        console.log('Connected to database\n');
     }catch(error) {
         console.log('Error connecting to database:', error);
     };
