dcf845a586d486f4ed334799ac2d1eef7d6b6e7b
Giordanni <giordanniformiga103@gmail.com>
2025-04-15 18:21:27 -0300

    Revert "Feature/5 send code (#23)"
    This reverts commit d4ae8ed5cd48156ba6f9d81aa4bc7f323b3a936e.

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import { conectDataBase } from "./src/database/db.js";
-import { connectRedis } from "./src/database/redis.js";
 import userRoutes from "./src/routes/user_routes.js";
 
 dotenv.config();
 const app = express();
 
-conectDataBase();
-connectRedis();
-
+conectDataBase()
 app.use(express.json());
 
 app.use(userRoutes);
