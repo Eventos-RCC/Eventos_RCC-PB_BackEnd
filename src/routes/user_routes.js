7d1e9cd87cb56f66d944fb4ae88132de423dd2d2
giordanni <jordani.alformiga@gmail.com>
2025-04-15 18:29:25 -0300

    Revert "Feature/5 send code (#23)" (#25)
    This reverts commit d4ae8ed5cd48156ba6f9d81aa4bc7f323b3a936e.
    
    Co-authored-by: Giordanni <giordanniformiga103@gmail.com>
    
    Precisei reverter um merge

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -4,6 +4,5 @@ import userController from '../controllers/user_controller.js';
 const userRoute = Router();
 
 userRoute.post('/api/user', userController.create_user)
-userRoute.post('/api/CodeVerification', userController.CodeVerification)
 
 export default userRoute;
--- src/utils/basicFunctions.js
+++ src/utils/basicFunctions.js
@@ -1,12 +1,5 @@
 import logger from './logger.config.js';
 import userModels from '../models/user_models.js';
-import redis from '../models/userRedis.js';
-
-const generateCodeValdation = async () => {
-    logger.info('Generating code validation');
-    const randomPart = Math.floor(1000 + Math.random() * 900000).toString().trim();
-    return randomPart;
- }
 
 const generateRegistrationId = async (diocese_id) => {
     logger.info('Generating registration_id');
@@ -33,19 +26,7 @@ const formatDateForDatabase = (dateString) => {
     return `${year}-${month}-${day}`; // Retorna no formato yyyy-mm-dd
 };
 
-const verify_code = async (email, code) =>
-{
-    const stored_code = await redis.getData('verification_code', email)
-    if (!stored_code) {
-        return false
-    }
-
-    return stored_code.code === code ? true : false
-}
-
 export {
     generateRegistrationId,
-    formatDateForDatabase,
-    generateCodeValdation,
-    verify_code
+    formatDateForDatabase
 }
