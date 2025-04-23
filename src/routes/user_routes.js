288b0946af01a9d4cc9bf74c9bbdeea64ef9447d
Giordanni <giordanniformiga103@gmail.com>
2025-04-23 11:56:21 -0300

    ADD logical login

--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -3,7 +3,8 @@ import userController from '../controllers/user_controller.js';
 
 const userRoute = Router();
 
-userRoute.post('/api/user', userController.create_user)
-userRoute.post('/api/CodeVerification', userController.CodeVerification)
+userRoute.post('/user', userController.create_user)
+userRoute.post('/CodeVerification', userController.CodeVerification)
+userRoute.post('/login', userController.login)
 
 export default userRoute;
