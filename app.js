1cd0507abfbad0a1e01716f032ed0ac978d99853
Giordanni <giordanniformiga103@gmail.com>
2025-05-06 11:12:18 -0300

    importing cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { conectDataBase } from "./src/database/db.js";
 import { connectRedis } from "./src/database/redis.js";
 import mainRouter from './src/routes/main_routes.js';
@@ -8,6 +9,7 @@ import mainRouter from './src/routes/main_routes.js';
 dotenv.config();
 const app = express();
 
+app.use(cors());
 conectDataBase();
 connectRedis();
 
